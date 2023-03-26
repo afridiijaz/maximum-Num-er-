fb458a8ec0aab83de4fbc6becd2ebc600fe67930
Ijaz Ahmad Afridi <afridiijaz520@gmail.com>
2023-03-26 16:17:49 +0500

    initial commit

---
+++ large.js
@@ -0,0 +1,6 @@
+function pickLargestElement(contents){
+    return Math.max(...contents);
+}
+let array=[5,6,78,34,130,5,67,150];
+
+console.log('The greatest Number is , ', pickLargestElement(array));
